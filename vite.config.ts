56dab1e0651ff24b6ad376d0194b35d1b43eac9e
Wisdomdev <aguowisdom@gmail.com>
2026-02-17 11:01:36 +0100

    fix: explicit vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,11 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      server: {
+        preset: 'vercel',
+      },
+    }),
     viteReact(),
   ],
 })
