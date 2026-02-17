0ec77fada876e093e9d9e9f4f7182a7443dffb04
Wisdomdev <aguowisdom@gmail.com>
2026-02-17 10:44:40 +0100

    chore: remove netlify config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   resolve: {
@@ -15,7 +14,6 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    netlify(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
